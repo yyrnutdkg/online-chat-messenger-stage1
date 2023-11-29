7b4c40a1014ec5ef6474027692a697a177640c8a
yyrnutdkg <yyrnutdkg@gmail.com>
2023-11-30 01:14:19 +0900

    [add]serverをclassで表現

--- server.py
+++ server.py
@@ -3,64 +3,63 @@ import os
 import time
 import threading
 
-sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
-server_address = '/home/vagrant/work/recursion-stage1/udp_socket_file'
+class ChatServer:
+  def __init__(self, server_address):
+    self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+    self.server_address = server_address
+    self.connected_clients = []
+    self.message_last_time = {}
 
-connected_clients = []
-message_last_time = {}
+  def check_inactive_clients(self):
+    while True:
+      print('Start check user...')
+      current_time = time.time()
+      inactive_clients = []
 
-def check_inactive_clients():
-  while True:
-    print('start check user active!')
-    current_time = time.time()
-    inactive_clients = []
+      for client, last_time in self.message_last_time.items():
+        if current_time - last_time > 100:
+          self.inactive_clients.append(client)
 
-    for client, last_time in message_last_time.items():
-      if current_time - last_time > 100:
-        inactive_clients.append(client)
+      for del_client in inactive_clients:
+        print('Inactive user: {}'.format(del_client))
+        self.connected_clients.remove(del_client)
+        self.message_last_time.pop(del_client)
 
-    for del_client in inactive_clients:
-      print('非アクティブユーザ: {}'.format(del_client))
-      connected_clients.remove(del_client)
-      message_last_time.pop(del_client)
+      time.sleep(10)
 
-    time.sleep(10)
+  def start_server(self):
+    try:
+      os.unlink(self.server_address)
+    except FileNotFoundError:
+      pass
 
+    print('Starting up on {}'.format(self.server_address))
+    self.sock.bind(self.server_address)
+    check_user_thread = threading.Thread(target=self.check_inactive_clients)
+    check_user_thread.start()
 
+    self.handle_received_message()
 
-def start_server():
-  try:
-    os.unlink(server_address)
-  except FileNotFoundError:
-    pass
+  def handle_received_message(self):
+    while True:
+      print('Waiting to receive message....')
+      data, address = self.sock.recvfrom(4096)
 
-  print('starting up on {}'.format(server_address))
-  sock.bind(server_address)
+      data_decoded = data.decode('utf-8')
+      userlen, message = data_decoded.split(':')
 
-  check_user_thread = threading.Thread(target=check_inactive_clients)
-  check_user_thread.start()
+      self.connected_clients.append(address)
+      self.message_last_time[address] = time.time()
 
-  while True:
-    print('\nwaiting to receive message')
-    data, address = sock.recvfrom(4096)
+      print('Receicved message: {} from {}'.format(userlen, message))
 
-    connected_clients.append(address)
-    message_last_time[address] = time.time()
-    print(message_last_time)
-
-    data_str = data.decode('utf-8')
-    userlen, message = data_str.split(':')
-
-    print('received {} bytes from {}'.format(userlen, address))
-
-    print('received {}'.format(message))
-
-    if message:
-      message_encoded = message.encode('utf-8')
-      for client in connected_clients:
-        sent = sock.sendto(message_encoded, client)
-        print('sent {}  back to {}'.format(message_encoded, address))
-
+      if message:
+        message_encoded = message.encode('utf-8')
+        for client in self.connected_clients:
+          self.sock.sendto(message_encoded, client)
 
 if __name__ == "__main__":
-    start_server()
+  server_address = '/home/vagrant/work/online-chat-messenger-stage1/udp_socket_file'
+  chat_server = ChatServer(server_address)
+  server_thread = threading.Thread(target=chat_server.start_server)
+  server_thread.start()
